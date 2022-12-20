272920939b2e85d71e588b96bf85f797dc33fef4
MrCh1ppy <1762307994@qq.com>
2022-12-20 10:52:29 +0800

    迭代

--- src/main/java/com/ch1ppy/springboot_back_exam/pojo/bo/req/UserAddReq.java
+++ src/main/java/com/ch1ppy/springboot_back_exam/pojo/bo/req/UserAddReq.java
@@ -2,6 +2,9 @@ package com.ch1ppy.springboot_back_exam.pojo.bo.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author MrCh1ppy
  */
@@ -10,15 +13,18 @@ public class UserAddReq {
 	/**
 	 * 用户名
 	 */
+	@NotBlank
 	private String userName;
 
 	/**
 	 * 使用摘要算法处理后的密码，切忌使用密码原本存
 	 */
+	@NotBlank
 	private String passwordMd5;
 
 	/**
 	 * 一般使用假删除，通过修改一个flag位代表是否删除，0代表否，1代表是
 	 */
+	@NotNull
 	private Boolean isDelete;
 }
--- src/main/java/com/ch1ppy/springboot_back_exam/pojo/bo/req/UserPageQueryReq.java
+++ src/main/java/com/ch1ppy/springboot_back_exam/pojo/bo/req/UserPageQueryReq.java
@@ -2,12 +2,16 @@ package com.ch1ppy.springboot_back_exam.pojo.bo.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author MrCh1ppy
  */
 @Data
 public class UserPageQueryReq implements PageQueryReq {
+	@NotNull
 	int pageSize;
+	@NotNull
 	int pageNum;
 	String username;
 	Boolean isDelete;
